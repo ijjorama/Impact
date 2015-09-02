187b6217fffcfed4e928097350f50f4991a43a9b
ijjorama <ianjamesjohnson@gmail.com>
2015-09-02 14:33:58 +0100

    Revised search script for simple term

--- REF/refsearch-print-csids.py
+++ REF/refsearch-print-csids.py
@@ -9,46 +9,48 @@ def refSearch(phraseterm, proxies=None):
     url = svcbase + '?' + phraseterm + '&format=XML'
         
     response = requests.get(url, proxies=proxies)
-    return response.text 
-
-def getTagText(text, tag):
-    from xml.etree import cElementTree as ET
-
-    tagbase = '{http://schemas.datacontract.org/2004/07/REFAPIService}'
-    project = ET.fromstring(text)
-    tag = project.findall(tagbase + tag)
-    return tag[0].text
+    return response.text.encode("utf-8") 
 
 def getTagSingleText(elt, tagbase, tag):
-	elt = elt.findall(tagbase+tag)
-	return elt[0].text
-	
+
+    elt = elt.findall(tagbase+tag)
+    return elt[0].text
+
+def getCSIDs(root, tagbase, cstag):
+
+    csids = ()
+    for child in root:
+        csid = getTagSingleText(child, tagbase, cstag)
+        csids += (csid, )
+
+    return csids
+ 	
+def getCSIDsByPhrase(searchmethodurl, phraseterm, proxies=None):
+    pass
+
 def main():
  
     tagbase = '{http://schemas.datacontract.org/2004/07/REFAPIService}'
     cstag = 'CaseStudyId'
 
-    phrase = sys.argv[1] if len(sys.argv) == 2 else 'phrase=crystal' # 'phrase=crystal or mlcc'
+    phrase = sys.argv[1] if len(sys.argv) == 2 else 'phrase=crystal bridge' # 'phrase=crystal or mlcc'
     searchResults = refSearch(phrase, {"http": "http://wwwcache.rl.ac.uk:8080/"} )
     
     if len(searchResults) == 0:
         sys.exit("searchResults is zero-length")
-
-    searchResults = searchResults.encode("utf-8")
     
     root = ET.fromstring(searchResults)
     
-    if root is not None:
-        
-        if len( list(root) ) == 0:
-            print 'Fail'
-        else:
-            for child in root:
-                csid = getTagSingleText(child, tagbase, cstag) 
-                print 'CSID=%s' % csid
+    allcsids = ()
+    if len( list(root) ) != 0:
+        csids = getCSIDs(root, tagbase, cstag)
+        allcsids += (csids, )
+    allcsids = list(sum(allcsids,()))
 
-    else:
-    	print 'Root is None'
+    print 'Len(allcsids) = %d.\n' % len(allcsids)
+
+
+   
 
 if __name__ == "__main__":
     main()
